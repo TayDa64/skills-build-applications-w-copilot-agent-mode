aa67c583e9fedfbf56835b5efbaa1101491ea240
TayDa64 <TayDa64@users.noreply.github.com>
2025-04-23 18:38:47 +0000

    Add populate_db management command and test data for Octofit Tracker

---
+++ octofit-tracker/backend/octofit_tracker/test_data.py
@@ -0,0 +1,11 @@
+# Test data for Octofit Tracker
+
+test_users = [
+    {"_id": "1", "username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
+    {"_id": "2", "username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
+    {"_id": "3", "username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
+    {"_id": "4", "username": "crashoverride", "email": "crashoverride@hmhigh.edu", "password": "crashoverridepassword"},
+    {"_id": "5", "username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
+]
+
+# Add similar test data for teams, activities, leaderboard, and workouts as needed.
